d9a787f44ebf7a57023e920aa922213bf3fe6b1d
kamikase130 <jean-francois_papillon@hotmail.com>
2024-03-04 22:18:58 -0500

    test

--- jawhari-papillon/src/components/footer/Footer.jsx
+++ jawhari-papillon/src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="test">
-      <div className="">2024 Droit réserver à Jawarny et JF</div>
+      <div className="">2024 Droit réserver à Jawarny et JFP</div>
       <div className="">contactez nous : Jawarny@hotmail.com</div>
     </footer>
   );
